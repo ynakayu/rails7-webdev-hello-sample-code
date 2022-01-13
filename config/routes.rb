cd52d86284c5fae598b5bc3f266411b146651898
ynakayu <9111754+ynakayu@users.noreply.github.com>
2022-01-13 06:27:38 +0000

    S21-07-hello-world

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/index', to: 'users#index'
+  
+  root 'users#index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
